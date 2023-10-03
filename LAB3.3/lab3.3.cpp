36e0ce42839a90f0a87f3341a89f085fd66b58fa
Sofia-kosar <Sofiia.Kosar.RI.2023@lpnu.ua>
2023-10-03 15:52:01 +0300

    first commit

---
+++ LAB3.3/lab3.3.cpp
@@ -0,0 +1,35 @@
+// Lab_03_3.cpp
+// < Косар Софія Вікторівна >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 10
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+    double R; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x <= -8 - R)
+		y = R;
+	else
+		if (-8 - R  < x && x <= -8 + R)
+			y = R - sqrt( pow (R, 2) - pow ((x + 8), 2));
+		else
+			if (-8 + R < x && x <= -4)
+				y = R;
+			else
+				if (-4 < x && x <= 2)
+					y = ( -R - x - 4 - R * x ) / 3;
+				else
+					y = x - 3;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
